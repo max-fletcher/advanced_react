1e2923e12e72158c29450a69abf7081a453f2598
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2022-08-07 17:54:28 +0600

    Done upto 5:52.

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,54 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('Error...');
+	const [user, setUser] = useState('default user');
+
+	// fetch API works in a weird way. Don't ask. Better to use axios...
+	useEffect(() => {
+		fetch(url)
+			.then((response) => { 
+				if (response.status >= 200 && response.status <= 299) {
+					return response.json();
+				}
+				else {
+					setIsLoading(false);
+					setIsError(true);
+					throw new Error(response.statusText);
+				}
+			})
+			.then((some_user) => {
+				const { login } = some_user;
+				setUser(login);
+				setIsError(false);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div>
+				<h1> Loading... </h1>
+			</div>
+		)
+	} 
+	if(isError){
+		return (
+			<div>
+				<h1>{errorMessage}</h1>
+			</div>
+		)
+	}
+	return (
+		<div>
+			<h1>{user}</h1>
+		</div>
+	)
 };
 
 export default MultipleReturns;
