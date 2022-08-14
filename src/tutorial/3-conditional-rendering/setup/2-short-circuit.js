a142a36f73c829a6d5a0fea0db541037c6ddcb86
max-fletcher <amar.naam.machine@gmail.com>
2022-08-14 18:41:05 +0600

    Started 2-short-circuit in 3-conditional-rendering.

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,27 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
-	// const firstValue = text || 'hello world';
-	// const secondValue = text && 'hello world';
+	const [text1, settext1] = useState('')
+	const firstValue1 = text1 || 'hello world 1';
+	const secondValue1 = text1 && 'hello world 2';
 
-	return <h2>short circuit</h2>;
+	const [text2, settext2] = useState('dummy text')
+	const firstValue2 = text2 || 'hello world 3';
+	const secondValue2 = text2 && 'hello world 4';
+
+	return (
+		<>
+			<h2>short circuit</h2>
+
+			<h3>Text 1</h3>
+			<h4>{firstValue1}</h4>
+			<h4>{secondValue1}</h4>
+
+			<h3>Text 2</h3>
+			<h4>{firstValue2}</h4>
+			<h4>{secondValue2}</h4>
+		</>
+	)
 };
 
 export default ShortCircuit;
