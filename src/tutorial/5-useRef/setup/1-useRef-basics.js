65b81aab3b58a9c53dc0d7c1dcd5c93026de7989
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2022-09-24 05:23:06 +0600

    Done upto 7:29:50.

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,37 @@
 import React, { useEffect, useRef } from 'react';
 
-// preserves value
-// DOES NOT trigger re-render
+// preserves value (Hard Refresh does not preserve it though)
+// DOES NOT trigger re-render i.e on saving(Ctrl + s)
 // target DOM nodes/elements
+// works very similar to 'ref' prop in vue
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+
+  const refContainer = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Some Message');
+    console.log(refContainer.current.value);
+  }
+
+  useEffect(() => {
+    // used to focus the input field without causing re-render.
+    refContainer.current.focus();
+  })
+
+  return (
+    <>
+      <h2>useRef</h2>
+      <form className="form" onClick={handleSubmit}>
+        <div>
+          <label> Input Field </label>
+          <input type="text" ref={refContainer} />
+          <button type="submit"> Submit </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
