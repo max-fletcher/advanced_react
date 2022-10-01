5ca3286ca35b64449cae5aefb49f65e6dd7e919d
max-fletcher <amar.naam.machine@gmail.com>
2022-10-01 17:05:19 +0600

    Done upto 7:59:00.

--- src/tutorial/6-useReducer/setup/Modal.js
+++ src/tutorial/6-useReducer/setup/Modal.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react';
 
 const Modal = (props) => {
+  useEffect(()=>{
+    setTimeout(()=>{
+      props.close_modal()
+    }, 3000)
+  })
+
   return <div className='modal'>
     <p>
       {props.modalContent}
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,53 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 
 // reducer function
 const reducer = (state, actions) => {
+  console.log(state);
+  if(actions.type === "ADD_PERSON"){
+      const newPeople = [...state.people, actions.payload] // using spread operator to add person to the end of people array
 
+      // the object returned here is the new state. We are again using object destructuring to edit data before returning it. It is changing
+      // everything inside the initial state object.
+      return {
+        ...state,
+        people: newPeople,
+        isModalOpen: true,
+        modalContent: 'Person Added !'
+      }
+  }
+  else if(actions.type === "NO_VALUE"){
+      // the object returned here is the new state. We are again using object destructuring to edit data before returning it. It is changing
+      // everything except people.
+      return {
+        ...state,
+        isModalOpen: true,
+        modalContent: 'Please Enter Value Before Submitting !'
+      }
+  }
+  else if(actions.type === "CLOSE_MODAL"){
+      return {
+        ...state,
+        isModalOpen: false
+      }
+  }
+  else if(actions.type === "REMOVE_PERSON"){
+    const newPeople = state.people.filter(
+      (person) => {
+        return person.id !== actions.payload
+      }
+    )
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'Person Removed !'
+    }
+}
+  // better to throw an error to spot bugs. Returning a valid state by default is not good practice since it will fool you into thinking everything is OK.
+  throw new Error('No matching action type')
+  // return state  // returning this is not good practice as mentioned above
 }
 
 // default values for reducer
@@ -29,18 +72,27 @@ const Index = () => {
     e.preventDefault();
     console.log("form submitted");
     if(name){
-
+      const newPerson = {id: new Date().getTime().toString(), name: name}
+      // dispatch an action from reducer caled ADD_PERSON. From there, we need to return a new state. Not returning a new state will cause the reducer
+      // to throw errors.
+      // also appending a new person to the ADD_PERSON so it can be accessed from the reducer and then pushed to the data array.
+      dispatch({type : "ADD_PERSON", payload: newPerson})
+      setName('')
     }
     else{
-      
+      dispatch({type : "NO_VALUE"})
     }
+  }
+
+  const close_modal = () => {
+    dispatch({type : "CLOSE_MODAL"})
   }
 
   return(
     <>
       <h2>useReducer</h2>
       {/* passing state.modalContent into the modal so that  */}
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && <Modal closeModal={close_modal} modalContent={state.modalContent} />}
       <form className="form" onSubmit={handleSubmit}>
         <div>
           <input type="text" value={name} onChange={(e) => {setName(e.target.value)}} />
@@ -48,9 +100,13 @@ const Index = () => {
         <button type="submit"> Add Person </button>
       </form>
       {state.people.map((person) => {
-        return <div key={person.id}>
-          <h4>{person.name}</h4>
-        </div>
+        return(
+          <div key={person.id} className="item">
+            <h4>{person.name}</h4>
+            {/* dispatch here is used to close the modal using reducer described above */}
+            <button onClick={() => {dispatch({type: 'REMOVE_PERSON', payload: person.id})}}>Remove</button>
+          </div>
+        )
       })}
     </>
   );
